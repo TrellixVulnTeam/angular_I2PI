0bb093858d744a9b2ac1fd50d1e9d3695d4a0ef3
luispalominofrias <luis.palomino.frias@gmail.com>
2022-01-19 20:41:54 -0300

    clase 2

--- clase_01/class1/src/app/app.component.ts
+++ clase_01/class1/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component , OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //con este nombre se puede usar este componente en otros html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
